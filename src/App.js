f679b590e3952df773e972ae928c7daf68c8d957
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-01-29 00:51:28 +0100

    Update displayForm function to DisplayForm

--- src/App.js
+++ src/App.js
@@ -4,28 +4,53 @@ import './App.css';
 function App() {
   const [entries, setEntries] = useState([]);
   const addEntryToPhoneBook = (entry) => {
-    setEntries([...entries, entry].sort((a,b) => a.lastname.toLowerCase() > b.lastName.toLowerCase() ? 1 : -1));
+    setEntries(
+      [...entries, entry].sort((a,b) => 
+      a.lastname.toLowerCase() > b.lastName.toLowerCase() ? 1 : -1));
   }
   return (
     <div>
       <div className="container">
-       Hello, World
+        <DisplayForm  addEntryToPhoneBook={addEntryToPhoneBook}/>
       </div>
     </div>
     
   )
 }
 
-function EntryForm() {
-  const [firstName, setFirstName]    = useState("");
-  const [lastName, setLastname]      = useState("");
-  const [phoneNumer, setPhoneNumber] = useState("");
+function DisplayForm() {
+  const [firstName, setFirstName]     = useState("");
+  const [lastName, setLastName]       = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
   }
   return (
     <form onSubmit={handleSubmit}>
-
+      <input 
+        type="text"
+        name='firstName'
+        id='firstName'
+        placeholder='Firstname'
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input 
+        type="text"
+        name='lastName'
+        id='lastName'
+        placeholder='Lastname'
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <input 
+        type="text"
+        name='phoneNumber'
+        id='phoneNumber'
+        placeholder='Phone Number'
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+      />
     </form>
   );
 }
